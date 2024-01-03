013b23a7a611401465529e62cdff150881fb12ff
William Santana <williamandrade1058@gmail.com>
2024-01-03 15:17:17 -0300

    up

---
+++ ex01/ex01.py
@@ -0,0 +1,11 @@
+from math import pi
+
+name = 'William'
+age = 19
+
+print('Python na Escola de Programação da Alura')
+print(f'Meu nome é {name} e tenho {age} anos')
+
+print('a', 'l', 'u', 'r', 'a', sep='\n')
+
+print(f'O valor redondado de pi é {round(pi, 2)}')
---
+++ sabor-express/app.py
@@ -0,0 +1,54 @@
+import os
+
+
+def show_program_name():
+
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      
+      """)
+
+
+def show_options():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair \n')
+
+
+def finish_app():
+    os.system('cls')
+    print('Encerrando app')
+    print()
+
+
+def choose_option():
+    chosen_option = int(input('Escolha uma opção: '))
+    # choose_option = int(choose_option)
+
+    print(chosen_option == 1)
+    print(type(choose_option), type(1))
+
+    if chosen_option == 1:
+        print('Cadastrar restaurante')
+    elif chosen_option == 2:
+        print('Listar restaurante')
+    elif chosen_option == 3:
+        print('Ativar restaurante')
+    else:
+        finish_app()
+
+
+def main():
+    show_program_name()
+    show_options()
+    choose_option()
+
+
+if __name__ == '__main__':
+    main()
