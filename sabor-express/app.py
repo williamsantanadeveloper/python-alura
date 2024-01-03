0b3eec7a8ae06a6fbd493815c1f56e52d0063fe7
William Santana <williamandrade1058@gmail.com>
2024-01-03 20:50:26 -0300

    Utilizando try e except

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -27,24 +27,36 @@ def finish_app():
     print()
 
 
+def invalid_option():
+    print('Opção inválida!')
+    input('Digite uma tecla para voltar para o menu principal ')
+    main()
+
+
 def choose_option():
-    chosen_option = int(input('Escolha uma opção: '))
-    # choose_option = int(choose_option)
+    try:
+        chosen_option = int(input('Escolha uma opção: '))
+        # choose_option = int(choose_option)
 
-    print(chosen_option == 1)
-    print(type(choose_option), type(1))
+        # print(chosen_option == 1)
+        # print(type(choose_option), type(1))
 
-    if chosen_option == 1:
-        print('Cadastrar restaurante')
-    elif chosen_option == 2:
-        print('Listar restaurante')
-    elif chosen_option == 3:
-        print('Ativar restaurante')
-    else:
-        finish_app()
+        if chosen_option == 1:
+            print('Cadastrar restaurante')
+        elif chosen_option == 2:
+            print('Listar restaurante')
+        elif chosen_option == 3:
+            print('Ativar restaurante')
+        elif chosen_option == 4:
+            finish_app()
+        else:
+            invalid_option()
+    except:
+        invalid_option()
 
 
 def main():
+    os.system('cls')
     show_program_name()
     show_options()
     choose_option()
